34e0db890bdd588075021bf969f34125671a4b94
xiaoua <xiaoua@localhost>
2023-12-05 19:45:57 -0700

    add data

---
+++ README.md
@@ -0,0 +1,44 @@
+```
+xiaoua@node-1:~/tcp-client-server$ sudo taskset -c 1 ./client 
+Successfully built a connection with server!
+Variance: 29.205
+Average latency: 20.605
+90th tail latency: 20.707
+99th tail latency: 22.640
+99.9th tail latency: 31.186
+xiaoua@node-1:~/tcp-client-server$ sudo taskset -c 1 ./client 
+Successfully built a connection with server!
+Variance: 29.369
+Average latency: 20.620
+90th tail latency: 20.766
+99th tail latency: 22.551
+99.9th tail latency: 31.559
+xiaoua@node-1:~/tcp-client-server$ sudo taskset -c 1 ./client 
+Successfully built a connection with server!
+Variance: 27.042
+Average latency: 20.459
+90th tail latency: 20.585
+99th tail latency: 22.713
+99.9th tail latency: 31.810
+xiaoua@node-1:~/tcp-client-server$ sudo taskset -c 1 ./client 
+Successfully built a connection with server!
+Variance: 28.762
+Average latency: 20.525
+90th tail latency: 20.682
+99th tail latency: 22.754
+99.9th tail latency: 31.823
+xiaoua@node-1:~/tcp-client-server$ sudo taskset -c 1 ./client 
+Successfully built a connection with server!
+Variance: 28.339
+Average latency: 20.530
+90th tail latency: 20.657
+99th tail latency: 22.598
+99.9th tail latency: 31.350
+xiaoua@node-1:~/tcp-client-server$ sudo taskset -c 1 ./client 
+Successfully built a connection with server!
+Variance: 28.925
+Average latency: 20.525
+90th tail latency: 20.745
+99th tail latency: 22.552
+99.9th tail latency: 31.688
+```
--- client.c
+++ client.c
@@ -3,6 +3,30 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
+
+#define WARMUP 100000
+#define TOTAL_ROUND 2000000
+
+long arr[TOTAL_ROUND + 5];
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+void queryStatistic(long a[]) {
+    int n = a[0];
+    qsort(a + 1, n, sizeof(a[0]), cmpfunc);
+    double sum = 0.0;
+    for(int i = 1; i <= n; ++i) sum += a[i];
+    double var = 0.0;
+    for(int i = 1; i <= n; ++i) var += ((double)a[i] * 0.001)*(double)(a[i] * 0.001);
+    printf("Variance: %.3f\n", (var - (sum * 0.001) / n * (sum * 0.001)) / n);
+
+    printf("Average latency: %.3f\n", sum / n * 0.001);
+    printf("90th tail latency: %.3f\n", a[(long)(n * 0.9)] * 0.001);
+    printf("99th tail latency: %.3f\n", a[(long)(n * 0.99)] * 0.001);
+    printf("99.9th tail latency: %.3f\n", a[(long)(n * 0.999)] * 0.001);
+}
 
 int main() {
     int sockfd;
@@ -30,18 +54,27 @@ int main() {
 
     puts("Successfully built a connection with server!");
 
-    int epoch = 100000;
+    struct timespec start, end;
+
     char buff[1024];
-    uint32_t size = 512;
-    while (epoch --) {
+    uint32_t size = 32;
+    for (int i = 0; i < TOTAL_ROUND; ++i) {
+        clock_gettime(CLOCK_MONOTONIC, &start);
         uint32_t write_len = write(sockfd, buff, size);
-        printf("Sent %d bytes to server.\n", write_len);
+        // printf("Sent %d bytes to server.\n", write_len);
         uint32_t read_len = read(sockfd, buff, sizeof(buff));
-        printf("Received %d bytes from server.\n", read_len);
+        // printf("Received %d bytes from server.\n", read_len);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        if (i > WARMUP) {
+            arr[++arr[0]] = end.tv_sec * 1000000000LL + end.tv_nsec - start.tv_sec * 1000000000LL - start.tv_nsec;
+        }
     }
 
     // close the socket
     close(sockfd);
 
+    // statistic
+    queryStatistic(arr);
+    
     return 0;
 }
